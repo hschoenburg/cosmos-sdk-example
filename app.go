3a388a0621827e864435529f8f0a1a6bd97d8efa
hschoenburg <hschoenburg@gmail.com>
2019-04-22 15:47:06 -0700

    app.go - tie the pieces together

--- app.go
+++ app.go
@@ -1,13 +1,20 @@
 package main
 
 import (
-	"github.com/cosmos/cosmos-sdk/x/auth"
-  //"github.com/hschoenburg/nameshake"
-	"github.com/tendermint/tendermint/libs/log"
-  "github.com/cosmos/cosmos-sdk/codec"
-	//sdk "github.com/cosmos/cosmos-sdk/types"
 	bam "github.com/cosmos/cosmos-sdk/baseapp"
+	"github.com/cosmos/cosmos-sdk/codec"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/x/auth"
+	"github.com/cosmos/cosmos-sdk/x/bank"
+	"github.com/cosmos/cosmos-sdk/x/codec"
+	"github.com/cosmos/cosmos-sdk/x/dtsking"
+	"github.com/cosmos/cosmos-sdk/x/params"
+	"github.com/hschoenburg/cosmos-sdk-example/x/nameshake"
+	abci "github.com/tendermint/tendermint/abci/types"
+	cmn "github.com/tendermint/tendermint/libs/common"
 	dbm "github.com/tendermint/tendermint/libs/db"
+	"github.com/tendermint/tendermint/libs/log"
+	tmtypes "github.com/tendermint/tendermint/types"
 )
 
 const (
@@ -15,22 +22,155 @@ const (
 )
 
 type nameShakeApp struct {
-  BaseApp *bam.BaseApp
-  Cdc codec.Codec
+	BaseApp          *bam.BaseApp
+	cdc              *codec.Codec
+	keyMain          *sdk.KVStoreKey
+	keyAccount       *sdk.KVStoreKey
+	keyNS            *sdk.KVStoreKey
+	KeyFeeCollection *sdk.KVStoreKey
+	keyParams        *sdk.KVStoreKey
+	tkeyParams       *sdk.TransientStoreKey
+
+	accountKeeper       auth.AccountKeeper
+	bankKeeper          bank.Keeper
+	feeCollectionKeeper auth.FeeCollectionKeeper
+	paramsKeeper        params.Keeper
+	nsKeeper            nameservice.Keeper
 }
 
 func newNameShakeApp(logger log.Logger, db dbm.DB) *nameShakeApp {
 
-  // what is this can we explain it better?
+	// what is this can we explain it better?
 	cdc := MakeCodec()
 
 	bApp := bam.NewBaseApp(appName, logger, db, auth.DefaultTxDecoder(cdc))
 
 	var app = &nameShakeApp{
-		BaseApp: bApp,
-		Cdc:     cdc,
+		BaseApp:          bApp,
+		Cdc:              cdc,
+		keyMain:          sdk.NewKVStoreKey("main"),
+		keyAccount:       sdk.NewKVStoreKey("acc"),
+		keyNS:            sdk.NewKVStoreKey("ns"),
+		keyFeeCollection: sdk.NewKVStoreKey("fee_colletion"),
+		keyParams:        sdk.NewKVStoreKey("params"),
+		tkeyParams:       sdk.NewKVStoreKey("transient_params"),
 	}
 
-	return app
+	app.paramsKeeper = params.NewKeeper(app.cdc, app.keyParams, app.tkeyParams)
 
+	app.accountKeeper = auth.NewAccountKeeper(
+		app.cdc,
+		app.KeyAccount,
+		app.paramsKeeper.Subspace(auth.DefaultParamspace),
+		auth.ProtoBaseAcount,
+	)
+
+	app.bankKeeper = bank.NewBaseKeeper(
+		app.accountKeeper,
+		app.paramsKeeper.Subspace(bank.DefaultParamspace),
+		bank.DefaultCodespace,
+	)
+
+	app.feeCollectionKeeper = auth.NewFeeCollectionKeeper(cdc, app.keyfeeCollection)
+
+	app.nsKeeper = nameshake.NewKeeper(
+		app.bankKeeper,
+		app.keyNS,
+		app.cdc,
+	)
+
+	app.setAnteHandler(auth.NewAnteHandler(app.accountKeeper, app.feeCollectionKeeper))
+
+	app.Router().
+		AddRoute("bank", bank.NewHandler(app.bankKeeper)).
+		AddRoute("nameshake", nameshake.NewHandler(app.nsKeeper))
+
+	app.QueryRouter().
+		AddRoute("nameshake", nameshake.NewQuerier(app.nsKeeper)).
+		AddRoute("acc", auth.NewQuerier(app.accountKeeper))
+
+	app.SetInitChainer(app.initChainer)
+
+	app.MountStores(
+		app.keyMain,
+		app.keyAccount,
+		app.keyNS,
+		app.keyFeeCollection,
+		app.keyParams,
+		app.tkeyParams,
+	)
+
+	err := app.LoadLatestVersion(app.keyMain)
+	if err != nil {
+		cmn.Exit(err.Error())
+	}
+	return app
+}
+
+type GenesisState struct {
+	AuthData auth.GenesisState   `json:"auth"`
+	BankData bank.GenesisState   `json:"bank"`
+	Accounts []*auth.BaseAccount `json:"accounts"`
+}
+
+func (app *nameShakeApp) initChainer(ctx sdk.Context, req abci.RequestInitChain) abci.ResponseInitChain {
+
+	stateJSON := req.AppStateBytes
+
+	genesisState := new(GenesisState)
+
+	err := app.cdc.UnmarshalJSON(stateJSON, genesisState)
+
+	if err != nil {
+		panic(err)
+	}
+
+	for _, acc := range genesisState.Accounts {
+		acc.AccountNumber = app.acountKeeper.GetNextAccountNumber(ctx)
+		app.accountKeeper.SetAccount(ctx, acc)
+	}
+
+	auth.InitGenesis(ctx, app.accountKeeper, app.feeCollectionKeeper, genesisState.AuthData)
+	bank.initGenesis(ctx, app.bankKeeper, genesisState.BankData)
+
+	return abci.ResponseInitChain{}
+}
+
+func (app *nameShakeApp) ExportAppStateAndValidators() (appState json.RawMessage, validators []tmtypes.GenesisValidator, err error) {
+	ctx := app.NewContext(true, abci.Header{})
+	accounts := []*auth.BaseAccount{}
+
+	appendAccountsFn := func(acc auth.Account) bool {
+		account := &auth.BaseAccount{
+			Address: acc.GetAddress(),
+			Coins:   acc.GetCoins(),
+		}
+		accounts = append(accounts, account)
+		return false
+	}
+
+	app.accountKeeper.IterateAccounts(ctx, appendAccountsFn)
+
+	genState := GenesisState{
+		Accounts: accounts,
+		AuthData: auth.DefaultGenesisState(),
+		BankData: bank.DefaultGenesisState(),
+	}
+
+	appState, err = codec.MarshalJSONIndent(app.cdc, genState)
+	if err != nil {
+		return nil, nil, err
+	}
+	return appState, validators, err
+}
+
+func MakeCodec() *codec.Codec {
+	var cdc = codec.New()
+	auth.RegisterCoder(cdc)
+	bank.RegisterCodec(cdc)
+	nameshake.RegisterCodec(cdc)
+	staking.RegisterCodec(cdc)
+	sdk.RegisterCodec(cdc)
+	codec.RegisterCrypto(cdc)
+	return cdc
 }
--- x/nameshake/client/rest/rest.go
+++ x/nameshake/client/rest/rest.go
@@ -19,107 +19,127 @@ const (
 )
 
 func RegisterRoutes(cliCtx context.CLIContext, r *mux.Router, cdc *codec.Codec, storeName string) {
-  r.HandleFunc(fmt.Sprintf("/%s/names", storeName), buyNameHandler(cdc, cliCtx)).Methods("POST")
-  r.HandleFunc(fmt.Sprintf("/%s/names", storeName), setNameHandler(cdc, cliCtx)).Methods("PUT")
-  r.HandleFunc(fmt.Sprintf("/%s/names/{%s}", storeName, restName), resolveNameHandler(cdc, cliCtx, storeName)).Methods("GET")
-  r.HandleFunc(fmt.Sprintf("/%s/names/{%s}/whois", storeName, restName), WHoIsHandler(cdc, cliCtx, storeName)).Methods("GET")
+	r.HandleFunc(fmt.Sprintf("/%s/names", storeName), buyNameHandler(cdc, cliCtx)).Methods("POST")
+	r.HandleFunc(fmt.Sprintf("/%s/names", storeName), setNameHandler(cdc, cliCtx)).Methods("PUT")
+	r.HandleFunc(fmt.Sprintf("/%s/names/{%s}", storeName, restName), resolveNameHandler(cdc, cliCtx, storeName)).Methods("GET")
+	r.HandleFunc(fmt.Sprintf("/%s/names/{%s}/whois", storeName, restName), WHoIsHandler(cdc, cliCtx, storeName)).Methods("GET")
 }
 
 func resolveNameHandler(cdc *codec.Codec, cliCtx context.CLIContext, storeName string) http.HandlerFunc {
-  return func(q http.ResponseWriter, r *http.Request) {
-    vars := mux.Vars(r)
-    paramType := vars[restName]
+	return func(q http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		paramType := vars[restName]
 
-    res, err := cliCtx.QueryWithData(fmt.Sprintf("custom/%s/resolve/%s", storeName, paramType), nil)
-    if err != nil {
-      rest.WriteErrorResponse(w, http.StatusNotFound, err.Error())
-      return
-    }
+		res, err := cliCtx.QueryWithData(fmt.Sprintf("custom/%s/resolve/%s", storeName, paramType), nil)
+		if err != nil {
+			rest.WriteErrorResponse(w, http.StatusNotFound, err.Error())
+			return
+		}
 
-    rest.PostProcessResponse(w, cdc, res, cliCtx.Indent)
-  }
+		rest.PostProcessResponse(w, cdc, res, cliCtx.Indent)
+	}
 }
-
-
 
 func whoIsHandler(cdc *codec.Codec, cliCtx context.CLIContext, storeName string) http.HandlerFunc {
-  return func(q http.ResponseWriter, r *http.Request) {
-    vars := mux.Vars(r)
-    paramType := vars[restName]
+	return func(q http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		paramType := vars[restName]
 
-    res, err := cliCtx.QueryWithData(fmt.Sprintf("custom/%s/whois/%s", storeName, paramType), nil)
-    if err != nil {
-      rest.WriteErrorResponse(w, http.StatusNotFound, err.Error())
-      return
-    }
+		res, err := cliCtx.QueryWithData(fmt.Sprintf("custom/%s/whois/%s", storeName, paramType), nil)
+		if err != nil {
+			rest.WriteErrorResponse(w, http.StatusNotFound, err.Error())
+			return
+		}
 
-    rest.PostProcessResponse(w, cdc, res, cliCtx.Indent)
-  }
+		rest.PostProcessResponse(w, cdc, res, cliCtx.Indent)
+	}
 }
-
-
-
 
 func namesHandler(cdc *codec.Codec, cliCtx context.CLIContext, storeName string) http.HandlerFunc {
-  return func(q http.ResponseWriter, r *http.Request) {
+	return func(q http.ResponseWriter, r *http.Request) {
 
-    res, err := cliCtx.QueryWithData(fmt.Sprintf("custom/%s/names/%s", storeName), nil)
-    if err != nil {
-      rest.WriteErrorResponse(w, http.StatusNotFound, err.Error())
-      return
-    }
+		res, err := cliCtx.QueryWithData(fmt.Sprintf("custom/%s/names/%s", storeName), nil)
+		if err != nil {
+			rest.WriteErrorResponse(w, http.StatusNotFound, err.Error())
+			return
+		}
 
-    rest.PostProcessResponse(w, cdc, res, cliCtx.Indent)
-  }
+		rest.PostProcessResponse(w, cdc, res, cliCtx.Indent)
+	}
 }
 
-
 type buyNameReq struct {
-  BaseReq rest.BaseReq `json: base_req"`
-  Name string `json: name"`
-  Amount string `json: amount"`
-  Buyer string `json: buyer"`
+	BaseReq rest.BaseReq `json: base_req"`
+	Name    string       `json: name"`
+	Amount  string       `json: amount"`
+	Buyer   string       `json: buyer"`
 }
 
 func buyNameHandler(cdc *codec.Codec, cliCtx context.CLIContext) http.HandlerFunc {
-  return func(w http.ResponseWriter, r *http.Request) {
-    var req buyNameReq
+	return func(w http.ResponseWriter, r *http.Request) {
+		var req buyNameReq
 
-    if !rest.ReadRESTReq(w, r, cdc, &req) {
-      rest.WriteErrorResponse(w, http.StatusBadRequest, "failed to parse request")
-      return 
-    }
+		if !rest.ReadRESTReq(w, r, cdc, &req) {
+			rest.WriteErrorResponse(w, http.StatusBadRequest, "failed to parse request")
+			return
+		}
 
-    baseReq := req.BaseReq.Sanitize()
-    if !baseReq.ValidateBasic(w) {
-      rest.WriteErrorResponse(w, http.StatusBadRequest, "invalid request")
-      return 
-    }
+		baseReq := req.BaseReq.Sanitize()
+		if !baseReq.ValidateBasic(w) {
+			rest.WriteErrorResponse(w, http.StatusBadRequest, "invalid request")
+			return
+		}
 
-    addr, err := sdk.AccAddressFromBech32(req.Buyer)
-    if err != nil {
-      rest.WriteErrorResponse(w, http.StatusBadRequest, err.Error())
-    }
+		addr, err := sdk.AccAddressFromBech32(req.Buyer)
+		if err != nil {
+			rest.WriteErrorResponse(w, http.StatusBadRequest, err.Error())
+		}
 
-    coins, err := sdk.ParseCoins(req.Amount)
-    if err != nil {
-      rest.WriteErrorResponse(w, http.StatusBadRequest, err.Error())
-    }
+		coins, err := sdk.ParseCoins(req.Amount)
+		if err != nil {
+			rest.WriteErrorResponse(w, http.StatusBadRequest, err.Error())
+		}
 
-    msg := nameshake.NewMsgBuyName(req.Name, coins, addr)
-    err = msg.ValidateBasic()
-    if err != nil {
-      rest.WriteErrorResponse(w, http.StatusBadRequest, err.Error())
-      return
-    }
-    clientrest.WriteGenerateStdTxResponse(w, cdc, cliCtx, baseReq, []sdk.Msg{msg})
-  }
+		msg := nameshake.NewMsgBuyName(req.Name, coins, addr)
+		err = msg.ValidateBasic()
+		if err != nil {
+			rest.WriteErrorResponse(w, http.StatusBadRequest, err.Error())
+			return
+		}
+		clientrest.WriteGenerateStdTxResponse(w, cdc, cliCtx, baseReq, []sdk.Msg{msg})
+	}
 }
 
+type setNameReq struct {
+	BaseReq rest.BaseReq `json:"base_req"`
+	Name    string       `json:"name"`
+	Value   string       `json:"value"`
+	Owner   string       `json:"owner"`
+}
 
+func setNameHandler(cdc *codec.Codec, cliCtx context.CLIContext) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var req setNameReq
+		if !rest.ReadRESTReq(w, r, cdc, &req) {
+			rest.WriteErrorResponse(w, htttp.StatusBadRequest, "failed to process request")
+			return
+		}
 
+		baseReq := req.BaseReq.Sanitize()
 
+		addr, err := sdk.AccAddressFromBech32(req.Owner)
+		if err != nil {
+			rest.WriteErrorResponse(w, http.StatusBadRequest, err.Error())
+			return
+		}
 
+		msg := nameshake.NewMsgSetName(req.Name, req.Value, addr)
+		err := msg.ValidateBasic()
+		if err != nil {
+			rest.WriteErrorResponse(w, http.StatusBadRequest, err.Error())
+			return
+		}
 
-
-
+		clientrest.WriteGenerateStdTxResponse(w, cdc, cliCtx, baseReq, []sdk.Msg{msg})
+	}
+}
